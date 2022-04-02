865b3d01cdc8ae41124f247d671af38b343efe05
Ilya Astapov <iastapov114@gmail.com>
2022-04-02 17:50:51 +0300

    +List and part of BinaryTree

---
+++ BinaryTree/BinaryTree.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class BinaryTree
+{
+private:
+    template<typename T>
+    class Node
+    {
+    public:
+        T key;
+        Node<T>* left;
+        Node<T>* right;
+        Node<T>* parent;
+
+        Node(T key, Node* parent = nullptr, Node* left = nullptr, Node* right = nullptr) {
+            this->key = key;
+            this->left = left;
+            this->right = right;
+            this->parent = parent;
+        }
+    };
+    int size;
+    Node<T>* head;
+
+    void print_elem(Node<T>* current);
+
+public:
+    BinaryTree();
+    ~BinaryTree();
+
+    void get_size();
+    bool find(T key);
+    void insert(T key);
+    void print();
+    void remove(T key);
+};
+
+
+template<typename T>
+void BinaryTree<T>::print_elem(Node<T>* current)
+{
+    if (current) {
+        print_elem(current->left);
+        cout << current->key << " ";
+        print_elem(current->right);
+    }
+    
+}   
+
+template<typename T>
+BinaryTree<T>::BinaryTree()
+{
+    head = nullptr;
+}
+
+template<typename T>
+BinaryTree<T>::~BinaryTree()
+{
+}
+
+template<typename T>
+void BinaryTree<T>::get_size()
+{
+    cout << size << endl;;
+}
+
+template<typename T>
+bool BinaryTree<T>::find(T key)
+{
+    Node<T>* current = this->head;
+    while (current) {
+        if (current->key == key) { return true; }
+        if (current->key > key) {
+            current = current->left;
+        }
+        else {
+            current = current->right;
+        }
+    }
+    return false;
+}
+
+template<typename T>
+void BinaryTree<T>::insert(T key)
+{
+    if (head != nullptr)
+    {
+        Node<T>* current = this->head;
+        while (current && current->key != key){
+            if (current->key > key && current->left == nullptr) {
+                current->left = new Node<T>(key);
+                size++;
+                return;
+            }
+            if (current->key < key && current->right == nullptr) {
+                current->right = new Node<T>(key);
+                size++;
+                return;
+            }
+            if (current->key > key) {
+                current = current->left;
+            }
+            else {
+                current = current->right;
+            }
+        }
+            
+    }
+
+
+    else { head = new Node<T>(key); size++; }
+}
+
+template<typename T>
+void BinaryTree<T>::print()
+{
+    cout << "Elements:  ";
+    print_elem(head);
+    cout << endl;
+}
+
+template<typename T>
+void BinaryTree<T>::remove(T key)
+{
+
+}
+
+
+int main()
+{
+    BinaryTree<int> a;
+    a.insert(5);
+    a.insert(6);
+    a.insert(1);
+    a.insert(2);
+    a.insert(3);
+    a.get_size();
+    a.print();
+}
---
+++ List(single)/List(single).cpp
@@ -0,0 +1,210 @@
+﻿#include <iostream>
+
+using namespace std;
+//Односвязный список
+template<typename T>
+class List
+{
+public:
+    List();
+    ~List();
+
+    void push_back(T data);
+    void push_first(T data);
+    void pop_back();
+    void pop_first();
+    void clear();
+    void print();
+    void insert(T data, int index);
+    void remove(int index);
+    int get_size() { return Size; };
+    T& operator [](const int index);
+
+private:
+    template<typename T>
+    class Node
+    {
+    public:
+        Node *pNext;
+        T data;
+
+        Node(T data = T(), Node *pNext = nullptr) {
+            this->data = data;
+            this->pNext = pNext;
+        }
+    };
+
+    int Size;
+    Node<T> *head;
+
+};
+
+
+
+
+
+template<typename T>
+List<T>::List()
+{
+    Size = 0;
+    head = nullptr;
+}
+
+template<typename T>
+List<T>::~List()
+{
+    cout << "Destructor" << endl;
+    clear();
+}
+
+template<typename T>
+void List<T>::push_back(T data)
+{
+    if (head == nullptr) {
+        head = new Node<T>(data);
+    }
+    else {
+        Node<T>* current = this->head;
+        while (current->pNext != nullptr) {
+            current = current->pNext;
+        }
+        current->pNext = new Node<T>(data);
+    }
+    Size++;
+}
+
+template<typename T>
+void List<T>::push_first(T data)
+{
+    head = new Node<T>(data, head);
+    Size++;
+}
+
+template<typename T>
+void List<T>::pop_back()
+{
+    if (Size == 1) {
+        pop_first();
+    }
+    else {
+        Node<T>* previous = this->head;
+        for (int i = 0; i < Size - 2; i++) {
+            previous = previous->pNext;
+        }
+
+        Node<T>* toDel = previous->pNext;
+
+        previous->pNext = nullptr;
+        delete toDel;
+        Size--;
+    }
+}
+
+template<typename T>
+void List<T>::pop_first()
+{
+    Node<T>* temp = head;
+    head = head->pNext;
+
+    delete temp;
+    Size--;
+}
+
+template<typename T>
+void List<T>::clear()
+{
+    while (Size) {
+        pop_first();
+    }
+}
+
+template<typename T>
+void List<T>::print()
+{
+    cout << "Elements : ";
+    if (Size>0) {
+        Node<T>* current = this->head;
+        while (current->pNext != nullptr) {
+            cout << current->data << " ";
+            current = current->pNext;
+        }
+        cout << current->data;
+    }
+    cout << endl;
+    
+}
+
+template<typename T>
+void List<T>::insert(T data, int index)
+{
+    if (index == 0) { push_first(data); }
+    else {
+        Node<T>* previous = this->head;
+        for (int i = 0; i < index - 1; i++) {
+            previous = previous->pNext;
+        }
+
+        previous->pNext = new Node<T>(data, previous->pNext);
+    }
+    Size++;
+}
+
+template<typename T>
+void List<T>::remove(int index)
+{
+    if (index == 0) { pop_first(); }
+    else {
+        Node<T>* previous = this->head;
+        for (int i = 0; i < index - 1; i++) {
+            previous = previous->pNext;
+        }
+
+        Node<T>* toDel = previous->pNext;
+
+        previous->pNext = toDel->pNext;
+
+        delete toDel;
+        Size--;
+    }
+}
+
+template<typename T>
+T& List<T>::operator[](const int index)
+{
+    int counter = 0;
+    Node<T>* current = this->head;
+    while (counter != index) {
+        current = current->pNext;
+        counter++;
+    }
+    return current->data;
+}
+
+
+
+int main()
+{
+    List<int> list;
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+    list.push_first(1);
+    list.insert(5, 2);
+   
+    list.print();
+
+    list.pop_first();
+    
+    list.print();
+    list.remove(1);
+    list.print();
+    list.pop_back();
+    list.print();
+    list.pop_back();
+    list.print();
+    list.pop_back();
+    list.print();
+
+
+     
+}
