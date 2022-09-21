260357be73bb72ddcb6be34aa4b2e410b11b9725
Ilya Astapov <iastapov114@gmail.com>
2022-09-21 18:31:06 +0300

    Create bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+enum class Color {
+    White = 0,
+    Gray = 1,
+    Black = 2,
+};
+
+std::queue<int> q;
+std::vector<Color> visited;
+
+
+void bfs(int v, const std::vector<std::vector<int>>& g)
+{
+    q.pop();
+
+    int size = g[v].size();
+    for (int i = 0; i < size; ++i) {
+        int u = g[v][i];
+        if (visited[u] == Color::White) {
+            q.push(u);
+            visited[u] = Color::Gray;
+        }
+    }
+    visited[v] = Color::Black;
+    std::cout << "Visited Vertex: " << v << std::endl;
+    while (!q.empty()) {
+        bfs(q.front(), g);
+    }
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::vector<std::vector<int>> graph(n);
+
+    int size;
+    int t;
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> size;
+        for (int j = 0; j < size; ++j) {
+            std::cin >> t;
+            graph[i].push_back(t);
+        }
+        visited.push_back(Color::White);
+    }
+
+    q.push(0);
+    visited[0] = Color::Gray;
+    bfs(0, graph);
+
+}
